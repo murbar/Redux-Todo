68d537f174d99cd7472a58367ea41328b63918aa
Joel Bartlett <joel@murbar.com>
2019-03-04 17:22:43 -0500

    Mock actions

---
+++ todo/src/actions/index.js
@@ -0,0 +1,10 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+export const addTodo = () => ({
+  type: ADD_TODO
+});
+
+export const toogleTodoCompleted = () => ({
+  type: TOGGLE_TODO
+});
