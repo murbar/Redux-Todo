072b97a11e3aa8fe207572751c29dbb21997a39c
Joel Bartlett <joel@murbar.com>
2019-03-05 18:35:25 -0500

    Edit title

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Redux Todo</h1>
+        <h1>Redux to-do list</h1>
         <TodoForm />
         <TodoList />
       </div>
