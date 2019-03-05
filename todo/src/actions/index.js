4a105fea6cc6e66e301c270b21847f5c1a513e0c
Joel Bartlett <joel@murbar.com>
2019-03-04 19:38:27 -0500

    Add logs

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,10 +1,16 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 
-export const addTodo = () => ({
-  type: ADD_TODO
-});
+export const addTodo = () => {
+  console.log('add');
+  return {
+    type: ADD_TODO
+  };
+};
 
-export const toggleTodoCompleted = () => ({
-  type: TOGGLE_TODO
-});
+export const toggleTodoCompleted = () => {
+  console.log('toggle');
+  return {
+    type: TOGGLE_TODO
+  };
+};
