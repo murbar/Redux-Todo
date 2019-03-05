521f094d49e5bb9527ece3a8edc3bcd4759719c0
Joel Bartlett <joel@murbar.com>
2019-03-04 20:00:49 -0500

    Finish TodoForm

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
 
-const TodoForm = props => {
-  console.log(props);
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  };
+
   return (
     <div className="todo-list">
-      <form onSubmit={props.addTodo}>
-        <input type="text" name="todo" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="todo" onChange={handleChange} value={value} />
         <button>Add</button>
       </form>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  };
-};
-
 export default connect(
-  mapStateToProps,
+  null,
   { addTodo }
 )(TodoForm);
