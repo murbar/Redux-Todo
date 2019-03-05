5741010a973092e940fc9422b4bb715cc99a3fdd
Joel Bartlett <joel@murbar.com>
2019-03-04 20:02:32 -0500

    Add ids, ADD_TODO

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,18 +1,34 @@
 import { ADD_TODO, TOGGLE_TODO } from '../actions';
 
+const newTodo = text => {
+  return {
+    id: Date.now(),
+    text
+  };
+};
+
 const initialState = {
-  todos: ['todo 1', 'todo 2']
+  todos: [
+    {
+      id: 1551746276929,
+      text: 'Todo 1'
+    },
+    {
+      id: 1551746441772,
+      text: 'Todo 2'
+    }
+  ]
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
       return {
-        todos: []
+        todos: [...state.todos, newTodo(action.text)]
       };
     case TOGGLE_TODO:
       return {
-        todos: []
+        todos: [...state.todos]
       };
     default:
       return state;
